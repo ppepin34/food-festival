729c27c43b17330b4fb386e0e0d436f77a05680f
ppepin34 <ppepin34@gmail.com>
2022-07-04 21:51:34 -0400

    service workers installed

--- webpack.config.js
+++ webpack.config.js
@@ -46,5 +46,12 @@ module.exports = {
             analyzerMode: 'static',
         })
     ],
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        static: {
+            directory:path.join(__dirname, '/'),
+        },
+        compress: true,
+        port: 8080,
+    }
 };
